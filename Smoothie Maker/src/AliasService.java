d588810584f3eb6342dc0b8fd9e8592bb6321d81
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-03-23 01:36:28 +0600

    initiated alias system

---
+++ Smoothie Maker/src/AliasService.java
@@ -0,0 +1,19 @@
+public class AliasService {
+    public static String getAlias(int score) {
+        if (score < -15) {
+            return "Health Novice";
+        } else if (score < -8) {
+            return "Wellness Trainee";
+        } else if (score < 0) {
+            return "Fitness Explorer";
+        } else if (score == 0) {
+            return "Health Enthusist";
+        } else if (score < 10) {
+            return "Wellness Seeker";
+        } else if (score < 20) {
+            return "Fitness Advocate";
+        } else {
+            return "Health Guru";
+        }
+    }
+}
